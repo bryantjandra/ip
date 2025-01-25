fb11be07409f475db98ed5d01f6d173c13bede5d
bryantjandra <tjandra.benedict@gmail.com>
2025-01-25 22:44:54 +0700

    Add storage and list functionality to store and display user input

--- src/main/java/Robert.java
+++ src/main/java/Robert.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class Robert {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
+        Scanner sc = new Scanner(System.in);
 
         System.out.println(line);
         System.out.println(" Hello! I'm Robert");
         System.out.println(" What can I do for you?");
         System.out.println(line);
 
-        Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         while (true) {
             String userCommand = sc.nextLine();
@@ -19,13 +21,23 @@ public class Robert {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
-            } else {
+
+            } else if (userCommand.equals("list")) {
                 System.out.println(line);
-                System.out.println(" " + userCommand);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(line);
+
+            } else {
+                tasks[taskCount] = userCommand;
+                taskCount++;
+
+                System.out.println(line);
+                System.out.println(" added: " + userCommand);
                 System.out.println(line);
             }
         }
-
         sc.close();
     }
 }
