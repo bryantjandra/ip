4300c9ad3e57884752fd1eeb624baf23976a5149
bryantjandra <tjandra.benedict@gmail.com>
2025-01-25 22:36:49 +0700

    Add echo functionality and handle bye command

--- src/main/java/Robert.java
+++ src/main/java/Robert.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class Robert {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
+
         System.out.println(line);
         System.out.println(" Hello! I'm Robert");
         System.out.println(" What can I do for you?");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String userCommand = sc.nextLine();
+
+            if (userCommand.equals("bye")) {
+                System.out.println(line);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            } else {
+                System.out.println(line);
+                System.out.println(" " + userCommand);
+                System.out.println(line);
+            }
+        }
+
+        sc.close();
     }
 }
