5b90a1b38c149c5f9c70079c74b3cdd43cb6483b
bryantjandra <tjandra.benedict@gmail.com>
2025-01-25 22:59:23 +0700

    Add Task class and integrate mark/unmark commands

--- src/main/java/Robert.java
+++ src/main/java/Robert.java
@@ -10,11 +10,11 @@ public class Robert {
         System.out.println(" What can I do for you?");
         System.out.println(line);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while (true) {
-            String userCommand = sc.nextLine();
+            String userCommand = sc.nextLine().trim();
 
             if (userCommand.equals("bye")) {
                 System.out.println(line);
@@ -24,13 +24,42 @@ public class Robert {
 
             } else if (userCommand.equals("list")) {
                 System.out.println(line);
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    Task t = tasks[i];
+                    System.out.println(" " + (i + 1) + ".["
+                            + t.getStatusIcon() + "] "
+                            + t.getDescription());
                 }
                 System.out.println(line);
 
+            } else if (userCommand.startsWith("mark ")) {
+                String[] parts = userCommand.split(" ");
+                int taskNum = Integer.parseInt(parts[1]);
+                int index = taskNum - 1;
+
+                tasks[index].markAsDone();
+
+                System.out.println(line);
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   [X] " + tasks[index].getDescription());
+                System.out.println(line);
+
+            } else if (userCommand.startsWith("unmark ")) {
+                String[] parts = userCommand.split(" ");
+                int taskNum = Integer.parseInt(parts[1]);
+                int index = taskNum - 1;
+
+                tasks[index].markAsNotDone();
+
+                System.out.println(line);
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   [ ] " + tasks[index].getDescription());
+                System.out.println(line);
+
             } else {
-                tasks[taskCount] = userCommand;
+                Task newTask = new Task(userCommand);
+                tasks[taskCount] = newTask;
                 taskCount++;
 
                 System.out.println(line);
@@ -38,6 +67,7 @@ public class Robert {
                 System.out.println(line);
             }
         }
+
         sc.close();
     }
 }
