d22e20bb29ea7d9c106d27c6f630135baf3135f3
bryantjandra <tjandra.benedict@gmail.com>
2025-01-31 22:48:58 +0700

    Implement saving of tasks upon shutdown and loading upon startup

--- src/main/java/Robert.java
+++ src/main/java/Robert.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -21,154 +22,162 @@ public class Robert {
         System.out.println(" What can I do for you?");
         System.out.println(line);
 
-        ArrayList<Task> tasks = new ArrayList<>();
+        Storage storage = new Storage("data/robert.txt");
+        ArrayList<Task> tasks;
+        try {
+            tasks = storage.load();
+        } catch (IOException e) {
+            tasks = new ArrayList<>();
+        }
 
         while (true) {
             String userCommand = sc.nextLine().trim();
-            if (userCommand.isEmpty()) {
-                try {
-                    throw new RobertException("OOPS!!! You typed an empty command!");
-                } catch (RobertException e) {
-                    System.out.println(e.getMessage());
-                    System.out.println(line);
-                }
-                continue;
-            }
-
-            String[] parts = userCommand.split(" ", 2);
-            CommandType commandType = CommandType.parseCommand(parts[0]);
-            String arguments = parts.length > 1 ? parts[1].trim() : "";
 
             try {
-                switch (commandType) {
-                    case BYE:
-                        System.out.println(" Bye. Hope to see you again soon!");
-                        System.out.println(line);
-                        sc.close();
-                        return;
+                if (userCommand.equals("bye")) {
+                    System.out.println(" Bye. Hope to see you again soon!");
+                    System.out.println(line);
+                    break;
 
-                    case LIST:
-                        System.out.println(" Here are the tasks in your list:");
-                        for (int i = 0; i < tasks.size(); i++) {
-                            System.out.println(" " + (i + 1) + "." + tasks.get(i));
-                        }
-                        System.out.println(line);
-                        break;
+                } else if (userCommand.equals("list")) {
+                    System.out.println(" Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(" " + (i + 1) + "." + tasks.get(i));
+                    }
+                    System.out.println(line);
 
-                    case MARK:
-                        if (arguments.isEmpty()) {
-                            throw new RobertException("Please specify which task to mark!");
-                        }
-                        int markIdx = Integer.parseInt(arguments);
-                        if (markIdx < 1 || markIdx > tasks.size()) {
-                            throw new RobertException("Task number is out of range!");
-                        }
-                        tasks.get(markIdx - 1).markAsDone();
-                        System.out.println(" Nice! I've marked this task as done:");
-                        System.out.println("   " + tasks.get(markIdx - 1));
-                        System.out.println(line);
-                        break;
+                } else if (userCommand.startsWith("mark ")) {
+                    String[] parts = userCommand.split(" ");
+                    if (parts.length < 2) {
+                        throw new RobertException("Please specify which task to mark!");
+                    }
+                    int taskNum = Integer.parseInt(parts[1]);
+                    if (taskNum < 1 || taskNum > tasks.size()) {
+                        throw new RobertException("Task number is out of range!");
+                    }
+                    tasks.get(taskNum - 1).markAsDone();
+                    storage.save(tasks);
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println("   " + tasks.get(taskNum - 1));
+                    System.out.println(line);
 
-                    case UNMARK:
-                        if (arguments.isEmpty()) {
-                            throw new RobertException("Please specify which task to unmark!");
-                        }
-                        int unmarkIdx = Integer.parseInt(arguments);
-                        if (unmarkIdx < 1 || unmarkIdx > tasks.size()) {
-                            throw new RobertException("Task number is out of range!");
-                        }
-                        tasks.get(unmarkIdx - 1).markAsNotDone();
-                        System.out.println(" OK, I've marked this task as not done yet:");
-                        System.out.println("   " + tasks.get(unmarkIdx - 1));
-                        System.out.println(line);
-                        break;
+                } else if (userCommand.startsWith("unmark ")) {
+                    String[] parts = userCommand.split(" ");
+                    if (parts.length < 2) {
+                        throw new RobertException("Please specify which task to unmark!");
+                    }
+                    int taskNum = Integer.parseInt(parts[1]);
+                    if (taskNum < 1 || taskNum > tasks.size()) {
+                        throw new RobertException("Task number is out of range!");
+                    }
+                    tasks.get(taskNum - 1).markAsNotDone();
+                    storage.save(tasks);
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println("   " + tasks.get(taskNum - 1));
+                    System.out.println(line);
 
-                    case DELETE:
-                        if (arguments.isEmpty()) {
-                            throw new RobertException("Please specify which task to delete!");
-                        }
-                        int delIdx = Integer.parseInt(arguments);
-                        if (delIdx < 1 || delIdx > tasks.size()) {
-                            throw new RobertException("Task number is out of range!");
-                        }
-                        Task removedTask = tasks.remove(delIdx - 1);
-                        System.out.println(" Noted. I've removed this task:");
-                        System.out.println("   " + removedTask);
-                        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                        System.out.println(line);
-                        break;
+                } else if (userCommand.startsWith("delete ")) {
+                    String[] parts = userCommand.split(" ");
+                    if (parts.length < 2) {
+                        throw new RobertException("Please specify which task to delete!");
+                    }
+                    int taskNum = Integer.parseInt(parts[1]);
+                    if (taskNum < 1 || taskNum > tasks.size()) {
+                        throw new RobertException("Task number is out of range!");
+                    }
+                    Task removedTask = tasks.remove(taskNum - 1);
+                    storage.save(tasks);
+                    System.out.println(" Noted. I've removed this task:");
+                    System.out.println("   " + removedTask);
+                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println(line);
 
-                    case TODO:
-                        if (arguments.isEmpty()) {
-                            throw new RobertException("OOPS!!! The description of a todo should not be empty.");
-                        }
-                        Todo newTodo = new Todo(arguments);
-                        tasks.add(newTodo);
-                        System.out.println(" Got it. I've added this task:");
-                        System.out.println("   " + newTodo);
-                        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                        System.out.println(line);
-                        break;
+                } else if (userCommand.startsWith("todo")) {
+                    String description = userCommand.substring("todo".length()).trim();
+                    if (description.isEmpty()) {
+                        throw new RobertException("OOPS!!! The description of a todo should not be empty.");
+                    }
+                    Todo newTodo = new Todo(description);
+                    tasks.add(newTodo);
+                    storage.save(tasks);
+                    System.out.println(" Got it. I've added this task:");
+                    System.out.println("   " + newTodo);
+                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println(line);
 
-                    case DEADLINE:
-                        if (!arguments.contains("/by")) {
-                            throw new RobertException("OOPS!!! A deadline must have '/by <time>'!");
-                        }
-                        String[] deadlineParts = arguments.split("/by");
-                        if (deadlineParts.length < 2) {
-                            throw new RobertException("OOPS!!! A deadline must have a description and a time after '/by'.");
-                        }
-                        String deadlineDesc = deadlineParts[0].trim();
-                        String byTime = deadlineParts[1].trim();
-                        if (deadlineDesc.isEmpty()) {
-                            throw new RobertException("OOPS!!! The description of a deadline cannot be empty.");
-                        }
-                        if (byTime.isEmpty()) {
-                            throw new RobertException("OOPS!!! The time of a deadline cannot be empty.");
-                        }
-                        Deadline newDeadline = new Deadline(deadlineDesc, byTime);
-                        tasks.add(newDeadline);
-                        System.out.println(" Got it. I've added this task:");
-                        System.out.println("   " + newDeadline);
-                        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                        System.out.println(line);
-                        break;
+                } else if (userCommand.startsWith("deadline")) {
+                    String withoutKeyword = userCommand.substring("deadline".length()).trim();
+                    if (withoutKeyword.isEmpty()) {
+                        throw new RobertException("OOPS!!! The description of a deadline cannot be empty.");
+                    }
+                    if (!withoutKeyword.contains("/by")) {
+                        throw new RobertException("OOPS!!! A deadline must have '/by <time>'!");
+                    }
+                    String[] parts = withoutKeyword.split("/by");
+                    if (parts.length < 2) {
+                        throw new RobertException("OOPS!!! A deadline must have a description and a time after '/by'.");
+                    }
+                    String description = parts[0].trim();
+                    String by = parts[1].trim();
+                    if (description.isEmpty()) {
+                        throw new RobertException("OOPS!!! The description of a deadline cannot be empty.");
+                    }
+                    if (by.isEmpty()) {
+                        throw new RobertException("OOPS!!! The time of a deadline cannot be empty.");
+                    }
+                    Deadline newDeadline = new Deadline(description, by);
+                    tasks.add(newDeadline);
+                    storage.save(tasks);
+                    System.out.println(" Got it. I've added this task:");
+                    System.out.println("   " + newDeadline);
+                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println(line);
 
-                    case EVENT:
-                        if (!arguments.contains("/from") || !arguments.contains("/to")) {
-                            throw new RobertException("OOPS!!! An event must have '/from <start>' and '/to <end>'!");
-                        }
-                        String[] fromSplit = arguments.split("/from");
-                        if (fromSplit.length < 2) {
-                            throw new RobertException("OOPS!!! Missing '/from' portion for event.");
-                        }
-                        String eventDesc = fromSplit[0].trim();
-                        String fromAndTo = fromSplit[1].trim();
-                        String[] toSplit = fromAndTo.split("/to");
-                        if (toSplit.length < 2) {
-                            throw new RobertException("OOPS!!! Missing '/to' portion for event.");
-                        }
-                        String from = toSplit[0].trim();
-                        String to = toSplit[1].trim();
-                        if (eventDesc.isEmpty()) {
-                            throw new RobertException("OOPS!!! The description of an event cannot be empty.");
-                        }
-                        if (from.isEmpty() || to.isEmpty()) {
-                            throw new RobertException("OOPS!!! The start and end times for an event cannot be empty.");
-                        }
-                        Event newEvent = new Event(eventDesc, from, to);
-                        tasks.add(newEvent);
-                        System.out.println(" Got it. I've added this task:");
-                        System.out.println("   " + newEvent);
-                        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                        System.out.println(line);
-                        break;
+                } else if (userCommand.startsWith("event")) {
+                    String withoutKeyword = userCommand.substring("event".length()).trim();
+                    if (withoutKeyword.isEmpty()) {
+                        throw new RobertException("OOPS!!! The description of an event cannot be empty.");
+                    }
+                    if (!withoutKeyword.contains("/from") || !withoutKeyword.contains("/to")) {
+                        throw new RobertException("OOPS!!! An event must have '/from <start>' and '/to <end>'!");
+                    }
+                    String[] fromSplit = withoutKeyword.split("/from");
+                    if (fromSplit.length < 2) {
+                        throw new RobertException("OOPS!!! Missing '/from' portion for event.");
+                    }
+                    String description = fromSplit[0].trim();
+                    String fromAndTo = fromSplit[1].trim();
+                    String[] toSplit = fromAndTo.split("/to");
+                    if (toSplit.length < 2) {
+                        throw new RobertException("OOPS!!! Missing '/to' portion for event.");
+                    }
+                    String from = toSplit[0].trim();
+                    String to = toSplit[1].trim();
+                    if (description.isEmpty()) {
+                        throw new RobertException("OOPS!!! The description of an event cannot be empty.");
+                    }
+                    if (from.isEmpty() || to.isEmpty()) {
+                        throw new RobertException("OOPS!!! The start and end times for an event cannot be empty.");
+                    }
+                    Event newEvent = new Event(description, from, to);
+                    tasks.add(newEvent);
+                    storage.save(tasks);
+                    System.out.println(" Got it. I've added this task:");
+                    System.out.println("   " + newEvent);
+                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println(line);
 
-                    default:
-                        throw new RobertException("OOPS!!! What do you mean by that?");
+                } else if (userCommand.isEmpty()) {
+                    throw new RobertException("OOPS!!! You typed an empty command!");
+
+                } else {
+                    throw new RobertException("OOPS!!! What do you mean by that?");
                 }
             } catch (RobertException e) {
                 System.out.println(e.getMessage());
+                System.out.println(line);
+            } catch (IOException e) {
+                System.out.println("OOPS!!! Failed to save tasks to file!");
                 System.out.println(line);
             } catch (NumberFormatException e) {
                 System.out.println("OOPS!!! The task number must be a valid integer!");
@@ -178,5 +187,7 @@ public class Robert {
                 System.out.println(line);
             }
         }
+
+        sc.close();
     }
 }
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,4 +1,64 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Storage {
+    private final String filePath;
+
+    public Storage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public ArrayList<Task> load() throws IOException {
+        ArrayList<Task> tasks = new ArrayList<>();
+        File f = new File(filePath);
+        if (!f.exists()) {
+            f.getParentFile().mkdirs();
+            f.createNewFile();
+            return tasks;
+        }
+        Scanner sc = new Scanner(f);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] parts = line.split("\\|");
+            String type = parts[0].trim();
+            String doneFlag = parts[1].trim();
+            if (type.equals("T")) {
+                Todo t = new Todo(parts[2].trim());
+                if (doneFlag.equals("1")) {
+                    t.markAsDone();
+                }
+                tasks.add(t);
+            } else if (type.equals("D")) {
+                Deadline d = new Deadline(parts[2].trim(), parts[3].trim());
+                if (doneFlag.equals("1")) {
+                    d.markAsDone();
+                }
+                tasks.add(d);
+            } else if (type.equals("E")) {
+                Event e = new Event(parts[2].trim(), parts[3].trim(), parts[4].trim());
+                if (doneFlag.equals("1")) {
+                    e.markAsDone();
+                }
+                tasks.add(e);
+            }
+        }
+        return tasks;
+    }
+
+    public void save(ArrayList<Task> tasks) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        for (Task t : tasks) {
+            if (t instanceof Todo) {
+                fw.write("T|" + (t.isDone ? "1" : "0") + "|" + t.description + System.lineSeparator());
+            } else if (t instanceof Deadline d) {
+                fw.write("D|" + (d.isDone ? "1" : "0") + "|" + d.description + "|" + d.by + System.lineSeparator());
+            } else if (t instanceof Event e) {
+                fw.write("E|" + (e.isDone ? "1" : "0") + "|" + e.description + "|" + e.from + "|" + e.to + System.lineSeparator());
+            }
+        }
+        fw.close();
+    }
 }
